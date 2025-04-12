1870233a4bc7b1fb8b2bb7d3c675a8efff619f3a
CKirby21 <cadenkirby@outlook.com>
2025-04-12 13:12:57 -0500

    Update hosts for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,10 +79,10 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts = [
+    "bbt-rails.org",     # Allow requests from example.com
+    /.*\.bbt-rails\.org/ # Allow requests from subdomains like `www.example.com`
+  ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
