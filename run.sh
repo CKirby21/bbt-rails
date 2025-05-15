b2f4769d1bd93ebf897c98009432ca08347dbefb
CKirby21 <cadenkirby@outlook.com>
2025-05-14 21:06:01 -0500

    update run script

--- run.sh
+++ run.sh
@@ -19,6 +19,14 @@ export RAILS_MASTER_KEY
 #     useradd rails --uid 1000 --gid 1000 --create-home --shell /bin/bash && \
 #     chown -R rails:rails db log storage tmp
 # USER 1000:1000
+#
+CLOUDFLARE_IPS="173.245.48.0/20 103.21.244.0/22 103.22.200.0/22 103.31.4.0/22 141.101.64.0/18 108.162.192.0/18 190.93.240.0/20 188.114.96.0/20 197.234.240.0/22 198.41.128.0/17 162.158.0.0/15 104.16.0.0/13 104.24.0.0/14 172.64.0.0/13 131.0.72.0/22"
+
+for IP in $CLOUDFLARE_IPS; do
+    iptables -I INPUT -p tcp -m multiport --dports http,https -s "$IP" -j ACCEPT
+done
+
+sudo socat TCP-LISTEN:80,reuseaddr,fork TCP:127.0.0.1:3000 &
 
 # Entrypoint prepares the database.
 # ENTRYPOINT ["/rails/bin/docker-entrypoint"]
