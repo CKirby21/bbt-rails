a2f033a02da350705cc1f6e4f440a1dc004ec6e3
CKirby21 <cadenkirby@outlook.com>
2025-05-16 12:00:51 -0500

    make validation for event score less strict

--- app/models/event_score.rb
+++ app/models/event_score.rb
@@ -2,7 +2,7 @@ class EventScore < ApplicationRecord
   belongs_to :team
   belongs_to :event
 
-  validates :score, presence: true, numericality: { greater_than: 0, less_than: 20 }
+  validates :score, presence: true, numericality: { greater_than: 0, less_than: 180 }
   validates :team_id, presence: true
   validates :event_id, presence: true
   validates :event_id, uniqueness: { scope: :team_id }
